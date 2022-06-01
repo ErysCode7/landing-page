a0a62dc4a96fcbfae9f748a84412498aab1550a0
Erys Mozo <mozoerys@gmail.com>
2022-06-01 19:03:00 +0700

    Modified Hamburger menu

--- app/js/index.js
+++ app/js/index.js
@@ -1,10 +1,14 @@
-import * as intersection from "./intersection-observer.js"; 
-import * as accordion from "./accordion.js";
 
 const hamburger = document.querySelector("[data-hamburger]")
+const hamburgerSpan = document.querySelectorAll("[data-hamburger-span]")
 const menu = document.querySelector("[data-menu]")
 
 
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("show")
+    hamburger.classList.toggle("not-active")
+    hamburger.classList.toggle("active")
+    hamburgerSpan.forEach(span => {
+        span.classList.toggle("active")
+    })
 })
